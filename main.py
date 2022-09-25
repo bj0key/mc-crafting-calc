8a7f5a40df16b0d4514ecf755225da1ce35e17eb
bjokey <>
2022-09-25 16:35:43 +0100

    Refactor and reformat most of the script, too lazy to document

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-from recipebook import recipebook
+from recipebook import recipebook as rb
 
-def get_materials(product):
+def get_materials(product: str) -> dict[str, int]:
     "Get the materials recursively required for an item."
-    if product in recipebook:
-        direct_recipe = recipebook[product] # the direct recipe for the item
-        total_materials = {}
-        for shallow_part, shallow_amount in direct_recipe.items(): # "shallow" this-level of recursion parts
-            indirect_recipe = get_materials(shallow_part)
-            for deep_part, deep_amount in indirect_recipe.items(): # "deep" already exhausted parts coming back up through the recursion
-                total_materials[deep_part] = total_materials.get(deep_part, 0) + deep_amount*shallow_amount 
-        return total_materials
-    else:
-        return {product:1}
+    if product not in rb:
+        return {product: 1}
+
+    direct_recipe = rb[product]  # the direct recipe for the item
+    total_materials = {}
+    for (shallow_part, shallow_amount) in direct_recipe.items():
+        indirect_recipe = get_materials(shallow_part)
+        for (deep_part, deep_amount) in indirect_recipe.items():
+            # "deep" already exhausted parts coming back up through the recursion
+            total_materials[deep_part] = (
+                total_materials.get(deep_part, 0) + deep_amount * shallow_amount
+            )
+    return total_materials
 
 
-def get_materials_pretty():
-    item = input("What item would you like the components for?\n")
-    print("="*64)
+def get_materials_pretty(item) -> str:
+    output = []
+    output.append(" [ MATERIALS ] ".center(64, "="))
     material_dict = get_materials(item)
     for material, amount in material_dict.items():
         stack, single = divmod(amount, 64)
-        print(material.ljust(32), f"{stack} stacks + {single}".ljust(24), f"({amount})")
+        stacks_str = f"{stack} stacks + {single}"
+        # print(material.ljust(32), f"{stack} stacks + {single}".ljust(24), f"({amount})")
+        output.append(f"{material:<32} {stacks_str:<24} ({amount})")
+    return "\n".join(output)
 
 
-def main():
-    get_materials_pretty()
-    pass
+def main() -> None:
+    item = input("What item would you like the components for?\n")
+    print(get_materials_pretty(item))
 
 
 if __name__ == "__main__":
     main()
-    input()
--- recipebook.py
+++ recipebook.py
@@ -1,57 +1,76 @@
-recipebook = {
-    # "silicon": {
-    #     "quartz":1
-    # },
-    # "quartz enriched iron": {
-    #     "iron ingot": 0.75,
-    #     "quartz": 0.25
-    # },
-    # "basic processor":{
-    #     "processor binding":1,
-    #     "silicon":1,
-    #     "redstone":1,
-    #     "iron ingot":1
-    # },
-    # "improved processor":{
-    #     "processor binding":1,
-    #     "silicon":1,
-    #     "redstone":1,
-    #     "gold ingot":1
-    # },
-    # "advanced processor":{
-    #     "processor binding":1,
-    #     "silicon":1,
-    #     "redstone":1,
-    #     "diamond":1
-    # },
-    "1k storage part":{
-        "redstone dust":1,
-        "glass":3,
-        "silicon":4,
-        "quartz enriched iron":1
+"""
+Here, I've provided an example set of recipe data, some RefinedStorage 
+storage parts. The entires within this recipe book 
+follows a fairly simple format:
+
+```py
+resultant_product:
+    { ingredient_1: amount_1,
+      ingredient_2, amount_2,
+      ...
+    }
+```
+
+The amount is always in terms of a single output product, e.g. 1 oak log 
+produces 4 planks, therefore a plank requires 0.25 logs
+"""
+from numbers import Number
+
+
+recipebook: dict[str, dict[str, Number]] = {
+    "silicon": {
+        "quartz": 1,
     },
-    "4k storage part":{
-        "redstone dust":1,
-        "1k storage part":3,
-        "basic processor":4,
-        "quartz enriched iron":1
+    "quartz enriched iron": {
+        "iron ingot": 0.75,
+        "quartz": 0.25,
     },
-    "16k storage part":{
-        "redstone dust":1,
-        "4k storage part":3,
-        "improved processor":4,
-        "quartz enriched iron":1
+    "basic processor": {
+        "processor binding": 1,
+        "silicon": 1,
+        "redstone": 1,
+        "iron ingot": 1,
     },
-    "64k storage part":{
-        "redstone dust":1,
-        "16k storage part":3,
-        "advanced processor":4,
-        "quartz enriched iron":1
+    "improved processor": {
+        "processor binding": 1,
+        "silicon": 1,
+        "redstone": 1,
+        "gold ingot": 1,
     },
-    "256k storage part":{
-        "redstone dust":1,
-        "64k storage part":3,
-        "advanced processor":4,
-        "quartz enriched iron":1
+    "advanced processor": {
+        "processor binding": 1,
+        "silicon": 1,
+        "redstone": 1,
+        "diamond": 1,
+    },
+    "1k storage part": {
+        "redstone dust": 1,
+        "glass": 3,
+        "silicon": 4,
+        "quartz enriched iron": 1,
+    },
+    "4k storage part": {
+        "redstone dust": 1,
+        "1k storage part": 3,
+        "basic processor": 4,
+        "quartz enriched iron": 1,
+    },
+    "16k storage part": {
+        "redstone dust": 1,
+        "4k storage part": 3,
+        "improved processor": 4,
+        "quartz enriched iron": 1,
+    },
+    "64k storage part": {
+        "redstone dust": 1,
+        "16k storage part": 3,
+        "advanced processor": 4,
+        "quartz enriched iron": 1,
+    },
+    "256k storage part": {
+        "redstone dust": 1,
+        "64k storage part": 3,
+        "advanced processor": 4,
+        "quartz enriched iron": 1,
     },
 }
